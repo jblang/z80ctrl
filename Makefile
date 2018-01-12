b6f2ec2f9068584c2c0d3c2db3b5cda9aa2666da
J.B. Langston <jb.langston@gmail.com>
2018-01-11 21:47:12 -0600

    Add fatfs

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 BIN=z80ctrl
-OBJS=z80ctrl.o uart.o spi.o iox.o bus.o
+OBJS=z80ctrl.o uart.o spi.o iox.o bus.o diskio.o ff.o ffsystem.o mmc_avr_spi.o ffunicode.o
 
 CC=avr-gcc
 OBJCOPY=avr-objcopy
@@ -9,7 +9,7 @@ F_CPU=20000000L
 PROGRAMMER=arduino
 PORT=/dev/ttyS9
 BAUD=115200
-CFLAGS=-O3 -DF_CPU=$(F_CPU) -mmcu=$(MCU)
+CFLAGS=-Os -DF_CPU=$(F_CPU) -mmcu=$(MCU) -I.
 
 
 $(BIN).hex: $(BIN).elf
--- z80ctrl.c
+++ z80ctrl.c
@@ -6,15 +6,15 @@
 #include <string.h>
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
 
 #include <util/delay.h>
 
 #include "uart.h"
 #include "bus.h"
+#include "ff.h"
 
 #include "hello.h"
-//#include "turnmon.h"
-//#include "basic4k40.h"
 #include "altmon.h"
 
 FILE uart_str = FDEV_SETUP_STREAM(uart_putchar, uart_getchar, _FDEV_SETUP_RW);
@@ -22,6 +22,37 @@ FILE uart_str = FDEV_SETUP_STREAM(uart_putchar, uart_getchar, _FDEV_SETUP_RW);
 #define WHITESPACE " \t\r\n"
 #define MAXBUF 80
 #define MAXARGS 3
+
+volatile UINT Timer;    /* Performance timer (100Hz increment) */
+FATFS fs;
+
+static
+void sd_init (void)
+{
+    FRESULT rc;
+
+    /* Start 100Hz system timer with TC0 */
+    OCR0A = F_CPU / 1024 / 100 - 1;
+    TCCR0A = _BV(WGM01);
+    TCCR0B = 0b101;
+    TIMSK0 = _BV(OCIE0A);
+
+    sei();
+    disk_initialize(0);
+    if ((rc = f_mount(&fs, "", 1)) != FR_OK) {
+        printf("error %d mounting fs.", rc);
+    }
+}
+
+ISR(TIMER0_COMPA_vect)
+{
+    Timer++;                        /* Performance counter for this module */
+    disk_timerproc();       /* Drive timer procedure of low level disk I/O module */
+}
+
+DWORD get_fattime (void) {
+    return 0;
+}
 
 void command_line(void) {
     char buf[MAXBUF];
@@ -68,17 +99,19 @@ int main(void)
 
     uart_init();
     stdout = stdin = &uart_str;
-    _delay_ms(500);
 
+    printf("Z80Ctrl 0.9 by J.B. Langston\n\n");
+
+    printf("initializing sd card...\n");
+    sd_init();
+    
+    printf("initializing bus...\n");
     bus_init();
-    printf("bus initialized: ");
-    bus_status();
 
-    printf("copying program...\n");
-    write_mem(0x0100, hello_bin, hello_bin_len);
+    printf("copying monitor...\n");
     write_mem(0xf800, altmon_bin, altmon_bin_len);
 
-    printf("running program...\n");
+    printf("starting monitor...\n");
     z80_run(0xf800);
 
     printf("halted.\n");
