53a8cd9287f26cfb9ca742d05749d56212e8728d
J.B. Langston <jb.langston@gmail.com>
2018-01-12 18:51:30 -0600

    Refactor dump_mem and verify_mem to use read_mem instead of directly maniuplate memory

--- memory.c
+++ memory.c
@@ -29,62 +29,6 @@ void read_mem(uint16_t addr, uint8_t *buf, uint16_t len)
     bus_slave();
 }
 
-// Verify specified number of bytes from external memory against a buffer
-uint8_t verify_mem(uint16_t addr, uint8_t *buf, uint16_t len, uint8_t log)
-{
-    uint8_t error = 0;
-    uint16_t i;
-
-    bus_master();
-    DATA_INPUT;
-    MREQ_LO;
-    RD_LO;
-    SET_ADDR(addr);
-    for (i = 0; i < len; i++) {
-        if (buf[i] != GET_DATA) {
-            if (log)
-                printf("Mismatch at %04x: expected %02x but read %02x\n", GET_ADDR, buf[i], GET_DATA);
-            error = 1;
-        }
-        addr++;
-        if ((addr & 0xFF) == 0) {
-            SET_ADDR(addr);
-        } else {
-            SET_ADDRLO(addr & 0xFF);
-        }
-    }
-    RD_HI;
-    MREQ_HI;
-    bus_slave();
-    return error;
-}
-
-// Hex dump memory block to console
-void dump_mem(uint16_t addr, uint16_t len)
-{
-    bus_master();
-    DATA_INPUT;
-    MREQ_LO;
-    RD_LO;
-    SET_ADDR(addr);
-    uint16_t i;
-    for (i = 0; i < len; i++) {
-        if ((i & 0xF) == 0)
-            printf("\n%04x: ", GET_ADDR);
-        printf("%02x ", GET_DATA);
-        addr++;
-        if ((addr & 0xFF) == 0) {
-            SET_ADDR(addr);
-        } else {
-            SET_ADDRLO(addr & 0xFF);
-        }
-    }
-    printf("\n");
-    RD_HI;
-    MREQ_HI;
-    bus_slave();
-}
-
 // Write specified number of bytes to external memory from a buffer
 void write_mem(uint16_t addr, uint8_t *buf, uint16_t len)
 {
@@ -107,4 +51,49 @@ void write_mem(uint16_t addr, uint8_t *buf, uint16_t len)
     MREQ_HI;
     DATA_INPUT;
     bus_slave();
+}
+
+// Verify specified number of bytes from external memory against a buffer
+int verify_mem(uint16_t start, uint8_t *src, uint16_t len, uint8_t log)
+{
+    uint8_t buf[256];
+    int errors = 0;
+    int i = 0, j = 0, buflen = 256;
+
+    while (i < len) {
+        if (len - i < buflen)
+            buflen = len - i;
+        read_mem(start+i, buf, buflen);
+        for (j = 0; j < buflen; i++, j++) {
+            if (buf[j] != src[i]) {
+                if (log)
+                    printf("Mismatch at %04x: expected %02x but read %02x\n", start+i, src[i], buf[j]);
+                errors++;
+            }
+        }
+    }
+    return errors;
+}
+
+// Hex dump memory block to console
+void dump_mem(uint16_t addr, uint16_t len)
+{
+    uint8_t buf[16];
+    int i = 0, j = 0, buflen = 16;
+
+    while (i < len) {
+        printf("%04X: ", addr + i);
+        read_mem(addr + i, buf, buflen);
+        for (j = 0; j < buflen; j++) {
+            printf("%02X ", buf[j]);
+        }
+        printf("  ");
+        for (j = 0; j < buflen; j++, i++) {
+            if (0x20 <= buf[j] && buf[j] <= 0x7e)
+                printf("%c", buf[j]);
+            else
+                printf(".");
+        }
+        printf("\n");
+    }
 }
