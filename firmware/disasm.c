b12efa9720f549f349043bfd9e3b2b5188ad7ed4
J.B. Langston <jb.langston@gmail.com>
2020-07-22 19:44:41 -0500

    Remove extra copyright comment

--- firmware/disasm.c
+++ firmware/disasm.c
@@ -1,5 +1,5 @@
 /* z80ctrl (https://github.com/jblang/z80ctrl)
- * Copyright 2018 J.B. Langston
+ * Copyright 2018-2020 J.B. Langston
  *
  * Permission is hereby granted, free of charge, to any person obtaining a 
  * copy of this software and associated documentation files (the "Software"), 
@@ -34,32 +34,6 @@
  * This disassembler decodes Z80 instructions using techniques described 
  * in http://www.z80.info/decoding.htm.
  */
-
-/* z80ctrl (https://github.com/jblang/z80ctrl)
- * Copyright 2018 J.B. Langston
- *
- * Permission is hereby granted, free of charge, to any person obtaining a 
- * copy of this software and associated documentation files (the "Software"), 
- * to deal in the Software without restriction, including without limitation 
- * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
- * and/or sell copies of the Software, and to permit persons to whom the 
- * Software is furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
- * DEALINGS IN THE SOFTWARE.
- */
-
-#include <stdint.h>
-#include <stdio.h>
-#include <avr/pgmspace.h>
 
 /*
  Tokens for each assembly mnemonic and operand are defined as follows:
